8cf613b075cb5d910befbddaee09f394eca35f93
Ranikhet <85451026+Ranikhet@users.noreply.github.com>
2023-02-16 11:29:38 +0530

    Add files via upload
    Unboxing concept

---
+++ Unbox.java
@@ -0,0 +1,15 @@
+
+public class Unbox 
+{
+	public static void main(String[] args) {
+
+		Integer a= new Integer(20);
+		int k=a.intValue();
+		System.out.println("conversion of wrapper to primitive explicitly"+k);
+		//int p=a;
+		//System.out.println("conversion of wrapper to primitive implicitly by compiler"+p);
+
+
+
+	}
+}
