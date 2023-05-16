40fbd0e61c3492a5d2bf1c1817a47eba8a05ccbe
Kireet Joshi <85451026+Ranikhet@users.noreply.github.com>
2023-05-16 23:22:06 +0530

    Add files via upload

---
+++ WinEvent.java
@@ -0,0 +1,79 @@
+/**
+ * @author Kireet
+ *
+ */
+import java.awt.*;
+import java.awt.event.*;
+public class WinEvent extends Frame implements WindowListener 
+{
+	Label l= new Label("name");
+	Button b= new Button("click me");
+	TextField tf= new TextField(15);
+	WinEvent()
+	{
+	add(l);
+	add(tf);
+	add(b);
+	setLayout(new FlowLayout());
+    setSize(250,100);
+    setVisible(true);
+    addWindowListener(this);
+ 
+	}
+	
+	
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		// TODO Auto-generated method stub
+		dispose();
+		System.out.println("closed");
+	}
+
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public static void main(String[] args) {
+		WinEvent we= new WinEvent();
+	}
+	
+}
